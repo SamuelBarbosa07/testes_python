dcba8ff7a5ddeca45d6a95fd1a5ca4ef26a52ebd
Samuel Barbosa <samuelbarbosa0717@gmail.com>
2023-09-09 10:52:56 -0300

    HELLO WORLD

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+
+nome = "Samuel"
+profissao = "aluno e programador"
+
+print(f"Este é o primeiro programa de {nome}")
+print("Eu sou", profissao, "e decidi trilhar os caminhos do python")
+
+resposta = input("Você está pronto para vir comigo? (S/N)")
