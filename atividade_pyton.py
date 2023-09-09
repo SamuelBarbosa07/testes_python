229ed416321672cce87e7c26ea5411a0b68a566f
Samuel Barbosa <samuelbarbosa0717@gmail.com>
2023-09-09 16:53:22 -0300

    operadores_aritméticos

---
+++ atividade_pyton.py
@@ -0,0 +1,29 @@
+#operadores aritimeticos 
+
+a = 7
+b = 2 
+c = 4 
+d = 14
+
+print(f"a+b-c, o resultado sera de {a+b-c}")
+
+print(f"a*b/c, o resultado sera de {a*b/c}")
+
+print(f"c+22*d, o resultado sera de {c+22*d}")
+
+print(f"a+b*c+d, o resultado sera de {a+b*c+d}")
+
+print(f"(a+b)*(c+d), o resultado sera de {(a+b)*(c+d)}")
+
+print(f"(d/a)/(b/c), o resultado sera de {(d/a)/(b/c)}")
+
+print(f"10**a, o resultado sera de {10**a}")
+
+print(f"58%a, o resultado sera de {58%a}")
+
+print(f"(a+b+c*d)/(b-c*(b-c)), o resultado sera de {(a+b+c*d)/(b-c*(b-c))}")
+
+print(f"(a+b+c*d)/(b-c*(b-c))+10**a, o resultado sera de {(a+b+c*d)/(b-c*(b-c))+10**a}")
+
+
+
