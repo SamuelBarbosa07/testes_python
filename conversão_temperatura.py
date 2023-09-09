e4913020b9aa0d16e731c313d9716bd7c573e991
Samuel Barbosa <samuelbarbosa0717@gmail.com>
2023-09-09 14:35:58 -0300

    calculo_temperatura

---
+++ conversão_temperatura.py
@@ -0,0 +1,13 @@
+print("===============================")
+print("=========CONVERSOR 7000========")
+print("===============================")
+
+celsius = float(input("Digite a temperatura em celsius"))
+
+fahre = celsius *1.8 +32
+kelvin = celsius +273.15
+
+print(f"fahrenheit {fahre}")
+print(f"kelvin {kelvin}")
+
+
